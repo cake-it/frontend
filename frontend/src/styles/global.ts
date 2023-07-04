b3e9f37d3c2e6793039149478656d30a36458a82
hyeone999 <caps026@naver.com>
2023-07-04 22:04:31 +0900

    design: global style(폰트, 컬러) 설정

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonView: {
+    alignItems: 'center',
+    gap: 20,
+    marginBottom: 20,
+  },
+});
