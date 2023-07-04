60d384e3e9c4eeac7f40ffa08a05d73408e79ccb
hyeone999 <caps026@naver.com>
2023-07-04 22:38:19 +0900

    design: 임시 Indicator 컴포넌트 생성(디자인 나오면 추후 구현 예정)

---
+++ frontend/src/components/Common/Indicator.tsx
@@ -0,0 +1,20 @@
+import { View } from 'react-native';
+import React from 'react';
+
+// 해당 컴포넌트는 아직 UI 구현되지 않았음 -> 추후 구현 예정
+const Indicator = () => {
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: 100,
+        backgroundColor: '#EDEDED',
+        marginTop: 20,
+      }}
+    />
+  );
+};
+
+export default Indicator;
+
+// const styles = StyleSheet.create({});
