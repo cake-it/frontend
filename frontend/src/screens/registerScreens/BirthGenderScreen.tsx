cffdd0094b1e4ae06d869901a5e5935ecbe57dfa
hyeone999 <caps026@naver.com>
2023-07-05 22:58:02 +0900

    feat: 생년월일 & 성별 입력 페이지 구현

---
+++ frontend/src/screens/registerScreens/BirthGenderScreen.tsx
@@ -0,0 +1,13 @@
+import BirthGenderInput from '@components/Register/BirthGenderInput';
+import Layout from '@components/Register/Layout';
+import React from 'react';
+
+const BirthGenderScreen = () => {
+  return (
+    <Layout>
+      <BirthGenderInput />
+    </Layout>
+  );
+};
+
+export default BirthGenderScreen;
