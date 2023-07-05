ad9c832ee6365e5585c951ae684ab57822c35335
hyeone999 <caps026@naver.com>
2023-07-05 22:58:19 +0900

    feat: 서비스 이용 체크 페이지 구현

---
+++ frontend/src/screens/registerScreens/CheckScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@components/Register/Layout';
+import Check from '@components/Register/Check';
+
+const CheckScreen = () => {
+  return (
+    <Layout>
+      <Check />
+    </Layout>
+  );
+};
+
+export default CheckScreen;
