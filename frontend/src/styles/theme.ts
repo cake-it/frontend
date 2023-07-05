41e2a3b8390d45b13c250d55d236b5ac499dc302
hyeone999 <caps026@naver.com>
2023-07-05 22:57:13 +0900

    feat: global style 및 theme 파일 수정

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -3,7 +3,12 @@ export const theme = {
   pink: '#FF648F',
   black: '#000000',
   white: '#FFFFFF',
+  disableTextGray: '#C0C0C0',
+  disableButtonGray: '#F4F4F4',
+  placeholderTextColor: '#DBDBDB',
 
   // font family
   bold: 'Pretendard-Bold',
+  medium: 'Pretendard-Medium',
+  regular: 'Pretendard-Regular',
 };
