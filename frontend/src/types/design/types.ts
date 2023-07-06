778967068baada02b15bc1d86433d6a9ee0c926f
hyeone999 <caps026@naver.com>
2023-07-06 13:54:37 +0900

    feat: 컴포넌트 type 추가

--- frontend/src/types/design/types.ts
+++ frontend/src/types/design/types.ts
@@ -1,8 +1,16 @@
+// onboarding button
 export interface OnboardingButtonProps {
   text: string;
   textColor: string;
   borderColor?: string;
   backgroundColor: string;
   disabled?: boolean;
+  onPress?: () => void;
+}
+
+// checkbox
+export interface CheckBox {
+  label: string;
+  checked: boolean;
   onPress?: () => void;
 }
