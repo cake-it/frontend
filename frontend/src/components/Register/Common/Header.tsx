6cad14d36388f09b33cc633c7dde7d781850aaa2
hyeone999 <caps026@naver.com>
2023-07-06 23:49:26 +0900

    feat: 공통 헤더 백 icon navigate 구현

--- frontend/src/components/Register/Common/Header.tsx
+++ frontend/src/components/Register/Common/Header.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Back from '@assets/images/common/backArrow.svg';
 import { theme } from 'styles/theme';
+import { useNavigation } from '@react-navigation/native';
 
 const Header = () => {
+  const navigation = useNavigation();
+
   return (
-    <View style={{ marginTop: 20, justifyContent: 'center' }}>
-      {/* svg pressed = 이전 화면으로 navigation.back() */}
-      <Back style={{ position: 'absolute', left: 20 }} />
+    <View style={styles.headerView}>
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => navigation.goBack()}
+      >
+        <Back />
+      </TouchableOpacity>
       <Text style={styles.headerText}>회원가입</Text>
     </View>
   );
@@ -16,6 +23,16 @@ const Header = () => {
 export default Header;
 
 const styles = StyleSheet.create({
+  headerView: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backButton: {
+    position: 'absolute',
+    left: 20,
+  },
   headerText: {
     fontSize: 14,
     fontFamily: theme.bold,
