62e21560bab557c74087254ec7f75691e29d082f
hyeone999 <caps026@naver.com>
2023-07-07 08:17:29 +0900

    feat: layout.tsx 코드 리뷰 수정사항 반영

--- frontend/src/components/Register/Layout.tsx
+++ frontend/src/components/Register/Layout.tsx
@@ -5,42 +5,44 @@ import {
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { theme } from 'styles/theme';
 import { globalStyles } from 'styles/global';
+import { LayoutProps } from 'types/design/types';
 import Header from '@components/Register/Common/Header';
 import Indicator from '@components/Register/Common/Indicator';
 import OnboardingButton from '@components/Register/Common/OnboardingButton';
 
-interface LayoutProps {
-  navigation?: any;
-  route?: any;
-  children: ReactNode;
-  profile?: boolean; // profile is checked
-  check?: boolean; // checkScreen is checked
-}
-
 const Layout = ({ children, profile, check, navigation }: LayoutProps) => {
-  // 프로필 설정, 서비스 체크 화면일 경우 scrollView 사용을 위해 TouchableWithoutFeedback 제거
-  return profile || check ? (
-    <View style={globalStyles.buttonFlex}>
-      <SafeAreaView style={globalStyles.container}>
-        <Header />
+  const isProfileOrCheck = profile || check;
 
-        <Indicator />
-        {children}
-      </SafeAreaView>
+  const renderButton = () => (
+    <OnboardingButton
+      disabled
+      backgroundColor={theme.disableButtonGray}
+      textColor={theme.disableTextGray}
+      text="완료하기"
+    />
+  );
+
+  const renderContent = () => (
+    <SafeAreaView style={globalStyles.container}>
+      <Header />
+      <Indicator />
+      {children}
+    </SafeAreaView>
+  );
+
+  // 프로필 설정, 서비스 체크 화면일 경우 scrollView 사용을 위해 TouchableWithoutFeedback 제거
+  return isProfileOrCheck ? (
+    <View style={globalStyles.buttonFlex}>
+      {renderContent()}
 
       <View style={globalStyles.buttonView}>
         <View style={styles.splitLine} />
         {/* 버튼 스타일 분기처리 */}
         {check ? (
-          <OnboardingButton
-            disabled
-            backgroundColor={theme.disableButtonGray}
-            textColor={theme.disableTextGray}
-            text="완료하기"
-          />
+          renderButton()
         ) : (
           <OnboardingButton
             disabled={false}
@@ -56,21 +58,11 @@ const Layout = ({ children, profile, check, navigation }: LayoutProps) => {
     // default button
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={globalStyles.buttonFlex}>
-        <SafeAreaView style={globalStyles.container}>
-          <Header />
-
-          <Indicator />
-          {children}
-        </SafeAreaView>
+        {renderContent()}
 
         <View style={globalStyles.buttonView}>
           <View style={styles.splitLine} />
-          <OnboardingButton
-            disabled
-            backgroundColor={theme.disableButtonGray}
-            textColor={theme.disableTextGray}
-            text="완료하기"
-          />
+          {renderButton()}
         </View>
       </View>
     </TouchableWithoutFeedback>
