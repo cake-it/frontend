8d9e7b3129d50874c2ee4b33fe3f990dc6fc26c2
hyeone999 <caps026@naver.com>
2023-07-15 13:35:38 +0900

    feat: Toast 메세지와 주변 애니메이션 구현

--- frontend/src/screens/CustomScreen.tsx
+++ frontend/src/screens/CustomScreen.tsx
@@ -1,22 +1,95 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Animated, Modal, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { globalStyles } from 'styles/global';
-import Header from '@components/Common/Header';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
+import { toastConfig } from '@components/Common/ToastComponent';
 import ImageComponent from '@components/Custom/ImageComponent';
 import CustomTabView from '@components/Custom/CustomTabView';
+import Header from '@components/Custom/Header';
 
 const CustomScreen = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [fadeAnim] = useState(new Animated.Value(0));
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  // 토스트 + 모달이 닫힐때 초간격으로 fade - out
+  const closeModal = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => {
+      setShowModal(false);
+    });
+  };
+
+  // modal 배경 애니메이션 (fade - in)
+  useEffect(() => {
+    if (showModal) {
+      showToast();
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+      setTimeout(() => {
+        closeModal();
+      }, 3000);
+    }
+  }, [showModal]);
+
+  // 토스트 메세지 show
+  const showToast = () => {
+    Toast.show({
+      position: 'top',
+      topOffset: 55,
+      visibilityTime: 3000,
+      type: 'codeToast',
+    });
+  };
+
   return (
-    <SafeAreaView style={globalStyles.container}>
-      <Header notBack title="커스텀 이미지 만들기" />
+    <View style={[globalStyles.container, { backgroundColor: '#FF815F' }]}>
+      <Header showToast={openModal} />
 
       <ImageComponent />
 
       <CustomTabView />
-    </SafeAreaView>
+
+      <Modal transparent visible={showModal} onRequestClose={closeModal}>
+        <Animated.View style={[styles.modalContainer, { opacity: fadeAnim }]} />
+        <Toast config={toastConfig} />
+      </Modal>
+    </View>
   );
 };
 
 export default CustomScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalText: {
+    fontSize: 24,
+    color: 'white',
+  },
+  closeButton: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: 'black',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
