6ae812441e9a1719df493aa56cda93d82fa38a99
hyeone999 <caps026@naver.com>
2023-07-16 12:49:52 +0900

    feat: 변경된 프로필 선택화면 인터렉션 적용

--- frontend/src/components/Register/ProfileSelect.tsx
+++ frontend/src/components/Register/ProfileSelect.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import { registerStyles } from 'styles/register/styles';
-import { ScrollView, Text, View } from 'react-native';
-import InfoLayout from './InfoLayout';
+import { Text, View } from 'react-native';
 import ProfileComponent from './ProfileComponent';
 import ProfileScrollComponent from './ProfileScrollComponent';
+import { globalStyles } from 'styles/global';
+import OnboardingButton from '@components/Common/OnboardingButton';
+import { theme } from 'styles/theme';
 
 // 페이지 렌더링되면 기본 사진 선택되어 있음? 아니면 선택 해야하는지?
-const ProfileSelect = () => {
+const ProfileSelect = ({ navigation }: { navigation: any; route: any }) => {
+  const textStyle = [
+    registerStyles.inputLabel,
+    {
+      marginLeft: 15,
+      paddingRight: 15,
+    },
+  ];
+
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <View style={registerStyles.inputContainer}>
-        <Text style={registerStyles.inputLabel}>프로필을 설정해주세요</Text>
+    <View style={globalStyles.buttonFlex}>
+      <View style={registerStyles.profileContainer}>
+        <Text style={textStyle}>프로필을 설정해주세요</Text>
 
         <ProfileComponent />
+
+        <View>
+          <ProfileScrollComponent />
+        </View>
       </View>
 
-      <ProfileScrollComponent />
-
-      <View style={[registerStyles.inputContainer, { marginBottom: 40 }]}>
-        <InfoLayout index={4} />
+      <View style={registerStyles.registerButtonView}>
+        <OnboardingButton
+          onPress={() => navigation.navigate('WelcomeScreen')}
+          backgroundColor={theme.pink}
+          textColor={theme.white}
+          text="완료하기"
+        />
       </View>
-    </ScrollView>
+    </View>
   );
 };
 
