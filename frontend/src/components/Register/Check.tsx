e7ee3850241510649047bd1014cbe4460ceee657
hyeone999 <caps026@naver.com>
2023-07-16 12:38:28 +0900

    design: 수정된 체크박스 페이지 인터렉션 구현

--- frontend/src/components/Register/Check.tsx
+++ frontend/src/components/Register/Check.tsx
@@ -1,50 +1,50 @@
 import React, { useState } from 'react';
 import Checkbox from '@components/design/CheckBox';
-import { Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { registerStyles } from 'styles/register/styles';
 import { checkBoxOptions } from 'utils/textRenderData/register';
-import InfoLayout from './InfoLayout';
 import { Props } from '@screens/registerScreens/CheckScreen';
+import OnboardingButton from '@components/Common/OnboardingButton';
+import { theme } from 'styles/theme';
+import { globalStyles } from 'styles/global';
 
 const Check = ({ navigation }: Props) => {
   const [checkedIndex, setCheckedIndex] = useState(-1);
+  const check = checkedIndex > -1;
 
   // 체크박스 선택 하나만
-  // 체크박스 선택시 다음 페이지로 navigate
   const handleCheck = (index: number) => {
     setCheckedIndex(index);
-    navigation.navigate('ProfileScreen');
   };
 
-  const renderContent = () => (
-    <View style={registerStyles.inputContainer}>
-      <Text style={registerStyles.inputLabel}>
-        서비스 이용 목적을 알려주세요
-      </Text>
+  return (
+    <View style={globalStyles.buttonFlex}>
+      <View style={registerStyles.inputContainer}>
+        <Text style={registerStyles.inputLabel}>
+          서비스 이용 목적을 알려주세요
+        </Text>
 
-      <View style={styles.checkBoxContainer}>
-        {checkBoxOptions.map((option, index) => (
-          <Checkbox
-            key={index}
-            label={option}
-            onPress={() => handleCheck(index)}
-            checked={checkedIndex === index}
-          />
-        ))}
+        <View style={styles.checkBoxContainer}>
+          {checkBoxOptions.map((option, index) => (
+            <Checkbox
+              key={index}
+              label={option}
+              onPress={() => handleCheck(index)}
+              checked={checkedIndex === index}
+            />
+          ))}
+        </View>
       </View>
 
-      <InfoLayout index={3} />
+      <View style={registerStyles.registerButtonView}>
+        <OnboardingButton
+          onPress={() => navigation.navigate('ProfileScreen')}
+          backgroundColor={check ? theme.pink : theme.disableButtonGray}
+          textColor={check ? theme.white : theme.disableTextGray}
+          text="다음"
+        />
+      </View>
     </View>
-  );
-
-  // 안드로이드일 경우 scrollView 설정을 위해
-  return Platform.OS === 'android' ? (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      {renderContent()}
-      <View style={{ marginBottom: 40 }} />
-    </ScrollView>
-  ) : (
-    renderContent()
   );
 };
 
