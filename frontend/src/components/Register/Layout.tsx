c067eeb6cec16188f09dcf4d391deaa2aa564c93
hyeone999 <caps026@naver.com>
2023-07-16 12:59:21 +0900

    desgin: 전체적인 디자인 수정에 따른 레이아웃 구조 변경

--- frontend/src/components/Register/Layout.tsx
+++ frontend/src/components/Register/Layout.tsx
@@ -1,35 +1,12 @@
-import {
-  Keyboard,
-  SafeAreaView,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import { Keyboard, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
 import React from 'react';
-import { theme } from 'styles/theme';
 import { globalStyles } from 'styles/global';
 import { RegisterLayoutProps } from 'types/design/types';
 import Header from '@components/Common/Header';
 import Indicator from '@components/Register/Common/Indicator';
-import OnboardingButton from '@components/Common/OnboardingButton';
 
-const Layout = ({
-  children,
-  profile,
-  check,
-  navigation,
-}: RegisterLayoutProps) => {
-  const isProfileOrCheck = profile || check;
-
-  const renderButton = () => (
-    <OnboardingButton
-      disabled
-      backgroundColor={theme.disableButtonGray}
-      textColor={theme.disableTextGray}
-      text="완료하기"
-    />
-  );
-
+const Layout = ({ children, profile }: RegisterLayoutProps) => {
+  // 기본 렌더링 되는 layout
   const renderContent = () => (
     <SafeAreaView style={globalStyles.container}>
       <Header title="회원가입" />
@@ -38,48 +15,14 @@ const Layout = ({
     </SafeAreaView>
   );
 
-  // 프로필 설정, 서비스 체크 화면일 경우 scrollView 사용을 위해 TouchableWithoutFeedback 제거
-  return isProfileOrCheck ? (
-    <View style={globalStyles.buttonFlex}>
-      {renderContent()}
-
-      <View style={globalStyles.buttonView}>
-        <View style={styles.splitLine} />
-        {/* 버튼 스타일 분기처리 */}
-        {check ? (
-          renderButton()
-        ) : (
-          <OnboardingButton
-            disabled={false}
-            onPress={() => navigation.navigate('WelcomeScreen')}
-            backgroundColor={theme.pink}
-            textColor={theme.white}
-            text="완료하기"
-          />
-        )}
-      </View>
-    </View>
+  // 프로필 설정일 경우 scrollView 사용을 위해 TouchableWithoutFeedback 제거
+  return profile ? (
+    renderContent()
   ) : (
-    // default button
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={globalStyles.buttonFlex}>
-        {renderContent()}
-
-        <View style={globalStyles.buttonView}>
-          <View style={styles.splitLine} />
-          {renderButton()}
-        </View>
-      </View>
+      {renderContent()}
     </TouchableWithoutFeedback>
   );
 };
 
 export default Layout;
-
-const styles = StyleSheet.create({
-  splitLine: {
-    borderWidth: 1,
-    width: '100%',
-    borderColor: '#EDEDED',
-  },
-});
