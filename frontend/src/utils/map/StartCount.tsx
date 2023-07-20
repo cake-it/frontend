7485500508d0a1211ececc46f289b43475325911
hyeone999 <caps026@naver.com>
2023-07-20 13:01:48 +0900

    feat: start count 기능 함수 구현

---
+++ frontend/src/utils/map/StartCount.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+
+import Star from '@assets/images/map/star.svg';
+import OffStar from '@assets/images/map/offStar.svg';
+
+export const StarRating = ({ stars }: { stars: number }) => {
+  // 별점에 따른 스타 배열 생성
+  const starElements = Array.from({ length: 5 }, (_, index) =>
+    index < stars ? <Star key={index} /> : <OffStar key={index} />
+  );
+
+  return <View style={{ flexDirection: 'row' }}>{starElements}</View>;
+};
