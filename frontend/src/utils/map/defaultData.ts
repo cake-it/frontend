6aba967a18e27e7be70873a3b4d8a93e0e52f801
hyeone999 <caps026@naver.com>
2023-07-21 11:08:46 +0900

    feat: default 위치 util 생성

---
+++ frontend/src/utils/map/defaultData.ts
@@ -0,0 +1,10 @@
+// delta 고정값
+export const delta = 0.002;
+
+// 위치 권한 동의하지 않은 경우 보여줄 위치
+export const defaultPlace = {
+  latitude: 37.541,
+  longitude: 126.986,
+  latitudeDelta: delta,
+  longitudeDelta: delta,
+};
