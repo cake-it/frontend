065c765e5c536abf839916fe600bc592abc81148
hyeone999 <caps026@naver.com>
2023-07-22 12:54:36 +0900

    comment: 공통-헤더 컴포넌트 props 주석 설명 추가

--- frontend/src/components/Common/Header.tsx
+++ frontend/src/components/Common/Header.tsx
@@ -7,17 +7,24 @@ import BottomArrow from '@assets/images/map/bottomArrow.svg';
 import { CommonHeaderProps } from 'types/design/types';
 
 const Header = ({ title, notBack, bottom, onPress }: CommonHeaderProps) => {
+  // title : 기본 헤더 제목
+  // notBack : true이면 뒤로 가는 interactions 없음
+  // bottom : bottomSheet
+  // 기본 action = 뒤로가기, onPress가 있으면? onPres
+
   const navigation = useNavigation();
 
   return (
     <View style={styles.headerView}>
-      <TouchableOpacity
-        activeOpacity={1.0}
-        style={styles.backButton}
-        onPress={onPress ? onPress : () => navigation.goBack()}
-      >
-        {bottom ? <BottomArrow /> : <Back />}
-      </TouchableOpacity>
+      {!notBack && (
+        <TouchableOpacity
+          activeOpacity={1.0}
+          style={styles.backButton}
+          onPress={onPress ? onPress : () => navigation.goBack()}
+        >
+          {bottom ? <BottomArrow /> : <Back />}
+        </TouchableOpacity>
+      )}
       <Text style={styles.headerText}>{title}</Text>
     </View>
   );
