a4cf9cbd8a4ab5dba4870ae3f310835091f249c0
hyeone999 <caps026@naver.com>
2023-07-22 12:50:18 +0900

    feat: iOS dark mode disable에 따른 status bar 처리

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,10 +5,16 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Platform, StatusBar } from 'react-native';
 import MainRoute from 'routes/MainRoute';
 
 const App = () => {
+  // 다크모드 UI가 없기 때문에 ios status bar 고정 설정
+  useEffect(() => {
+    StatusBar.setBarStyle(Platform.OS === 'ios' ? 'dark-content' : 'default');
+  }, []);
+
   return <MainRoute />;
 };
 
