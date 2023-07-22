82d9507d592339244446b0602cf2421168e32402
hyeone999 <caps026@naver.com>
2023-07-22 10:05:32 +0900

    design: 로그인 화면 온보딩 버튼 제거

--- frontend/src/components/Login/Layout.tsx
+++ frontend/src/components/Login/Layout.tsx
@@ -1,14 +1,11 @@
 import {
   Keyboard,
   SafeAreaView,
-  StyleSheet,
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import React from 'react';
 import Header from '@components/Common/Header';
-import OnboardingButton from '@components/Common/OnboardingButton';
-import { theme } from 'styles/theme';
 import { LoginLayoutProps } from 'types/design/types';
 import { globalStyles } from 'styles/global';
 
@@ -25,29 +22,9 @@ const Layout = ({ children, onPress, disabled }: LoginLayoutProps) => {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={globalStyles.buttonFlex}>
-        {renderContent()}
-        <View style={globalStyles.buttonView}>
-          <View style={styles.splitLine} />
-          <OnboardingButton
-            disabled={disabled}
-            onPress={onPress}
-            backgroundColor={disabled ? theme.disableButtonGray : theme.pink}
-            textColor={disabled ? theme.disableTextGray : theme.white}
-            text="로그인"
-          />
-        </View>
-      </View>
+      <View style={globalStyles.buttonFlex}>{renderContent()}</View>
     </TouchableWithoutFeedback>
   );
 };
 
 export default Layout;
-
-const styles = StyleSheet.create({
-  splitLine: {
-    borderWidth: 1,
-    width: '100%',
-    borderColor: '#EDEDED',
-  },
-});
