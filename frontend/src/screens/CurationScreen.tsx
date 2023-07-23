1307781580ffbb7b77fcc96f55ed6ff4b9e88a2a
hyeone999 <caps026@naver.com>
2023-07-23 10:34:04 +0900

    design: text 분기처리 및 안드로이드 사이즈 처리

--- frontend/src/screens/CurationScreen.tsx
+++ frontend/src/screens/CurationScreen.tsx
@@ -1,17 +1,26 @@
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import {
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import React, { useState } from 'react';
-import Header from '@components/Common/Header';
 import Card, { data } from '@components/Curation/Card';
 import { theme } from 'styles/theme';
 import Blog from '@components/Curation/Blog';
 
+// 플랫폼별 분기처리
+const cardBottom = Platform.OS === 'ios' ? 50 : 100;
+
 const CurationScreen = () => {
   const [blogPressed, setBlogPressed] = useState(false);
+
   return blogPressed ? (
     <Blog setBlogPressed={setBlogPressed} />
   ) : (
     <SafeAreaView style={styles.container}>
-      <Header title="큐레이션" notBack />
+      <Text style={styles.headerText}>큐레이션</Text>
 
       <ScrollView showsVerticalScrollIndicator={false} style={styles.marginTop}>
         {data.map((item, index) => (
@@ -22,7 +31,7 @@ const CurationScreen = () => {
             tag={item.tag}
             headText={item.headText}
             subText={item.subText}
-            bottom={index === data.length - 1 ? 50 : 20} // 마지막 카드는 marginBottom을 50으로 설정
+            bottom={index === data.length - 1 ? cardBottom : 20} // 마지막 카드는 marginBottom을 50으로 설정
           />
         ))}
       </ScrollView>
@@ -32,12 +41,22 @@ const CurationScreen = () => {
 
 export default CurationScreen;
 
+// 플랫폼별 분기처리
+const marginTop = Platform.OS === 'ios' ? 10 : 25;
+
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     backgroundColor: theme.white,
   },
   marginTop: {
-    marginTop: 30,
+    marginTop: 25,
+  },
+  headerText: {
+    color: theme.black,
+    fontFamily: theme.bold,
+    fontSize: 24,
+    marginLeft: 18,
+    marginTop: marginTop,
   },
 });
