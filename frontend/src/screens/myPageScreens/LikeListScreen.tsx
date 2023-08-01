8542a6d13bf957af6a26cc9fec441f812ca90432
hyeone999 <caps026@naver.com>
2023-08-01 18:26:24 +0900

    feat: 찜 페이지 임시 구현

---
+++ frontend/src/screens/myPageScreens/LikeListScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const LikeListScreen = () => {
+  return (
+    <View>
+      <Text>LikeListScreen</Text>
+    </View>
+  );
+};
+
+export default LikeListScreen;
+
+const styles = StyleSheet.create({});
