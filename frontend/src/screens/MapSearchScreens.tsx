47c0d370eff7c5d5e394ce0ce3378fd9ab2a4595
hyeone999 <caps026@naver.com>
2023-08-02 15:31:52 +0900

    feat: marker 구현

--- frontend/src/screens/MapSearchScreens.tsx
+++ frontend/src/screens/MapSearchScreens.tsx
@@ -1,43 +1,38 @@
-import {
-  Keyboard,
-  PermissionsAndroid,
-  Platform,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import { Keyboard, StyleSheet, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StackParamList } from 'types/routes/types';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import MapSearchComponent from '@components/Map/MapSearchComponent';
 import { Region } from 'types/map/types';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MapBottomSheet from '@components/Map/MapBottomSheet';
 import { defaultPlace, delta } from 'utils/map/defaultData';
+import DefaultMarker from '@assets/images/map/marker.svg';
+import PointMarker from '@assets/images/map/pointMarker.svg';
+import { requestPermission } from 'hooks/locationRequestPermission';
 type Props = NativeStackScreenProps<StackParamList, 'MapSearchScreen'>;
 
-// 권한 받아오기
-async function requestPermission() {
-  try {
-    if (Platform.OS === 'ios') {
-      return await Geolocation.requestAuthorization('always');
-    }
-    if (Platform.OS === 'android') {
-      return await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-      );
-    }
-  } catch (e) {
-    console.log(e);
-  }
-}
+// 임시 데이터
+const markersData = [
+  { latitude: 37.511007, longitude: 127.0940034 },
+  { latitude: 37.51181740000001, longitude: 127.0942317 },
+  { latitude: 37.5115088, longitude: 127.0936038 },
+  { latitude: 37.5113961, longitude: 127.0939363 },
+];
 
 const MapSearchScreens = ({ navigation }: Props) => {
   const [region, setRegion] = useState<Region | null>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [searchText, setSearchText] = useState('');
+
+  // marker 기능
+  const [selectedMarkerIndex, setSelectedMarkerIndex] = useState(-1);
+
+  const handleMarkerPress = (index: number) => {
+    setSelectedMarkerIndex(index);
+  };
 
   // 최초 로딩시 권한 요청 + 성공시 유저 위치 렌더링
   useEffect(() => {
@@ -67,8 +62,6 @@ const MapSearchScreens = ({ navigation }: Props) => {
     });
   }, []);
 
-  // console.log('검색한 위치 정보: ', region);
-
   return (
     <View style={styles.container}>
       <GestureHandlerRootView style={styles.container}>
@@ -79,7 +72,21 @@ const MapSearchScreens = ({ navigation }: Props) => {
           region={region || undefined}
           style={styles.container}
           provider={PROVIDER_GOOGLE}
-        />
+        >
+          {markersData.map((marker, index) => (
+            <Marker
+              key={index}
+              coordinate={marker}
+              onPress={() => handleMarkerPress(index)}
+            >
+              {selectedMarkerIndex === index ? (
+                <PointMarker style={styles.marker} />
+              ) : (
+                <DefaultMarker style={styles.marker} />
+              )}
+            </Marker>
+          ))}
+        </MapView>
 
         {/* Search */}
         <MapSearchComponent
@@ -102,5 +109,9 @@ export default MapSearchScreens;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  marker: {
+    width: 40,
+    height: 54,
   },
 });
